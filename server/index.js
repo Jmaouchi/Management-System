373b030f6e03137ff4df0345d8bdb266a88ae6a6
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-10-18 17:17:38 -0500

    set up express server and start graphQL

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema')
+require('dotenv').config();
+const port = process.env.PORT || 3002
+
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'developement',
+}))
+
+
+app.listen(port, console.log(`Server is running on ${port}`));
---
+++ server/schema/schema.js
@@ -0,0 +1,5 @@
+const { projects , clients} = require('../sampleData')
+
+const { graphQLObjectType} = require('graphql')
+
+// Clients Types
