f7ccd665a250f9e1d2f2a040ca5be3f9a7fdd248
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-10-18 17:48:14 -0500

    added a graphQL query to get a client by phone number

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,32 @@
 const { projects , clients} = require('../sampleData')
 
-const { graphQLObjectType} = require('graphql')
+const { graphQLObjectType, GraphQLObjectType, GraphQLString, graphql, GraphQLID, GraphQLSchema} = require('graphql')
 
-// Clients Types
+// Clients Types 
+const ClientType = new GraphQLObjectType ({
+  name: 'Client',
+  fields:() => ({
+    id:{type: GraphQLID},
+    name: {type: GraphQLString},
+    email: {type: GraphQLString},
+    phone: {type: GraphQLString}
+  })
+})
+
+const RootQuery = new GraphQLObjectType ({
+  name : "RootQueryType",
+  fields: {
+    client: {
+      type: ClientType,
+      args:{ id : {type: GraphQLID}},
+      resolve(parent , args){
+          return clients.find(client => client.id === args.id);
+      }
+    }
+  }
+})
+
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+})
