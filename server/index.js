a5018703a1479da720f21787b07f53b0c3150664
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-10-21 14:54:32 -0500

    Creating a mongoDB database and connect it

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const colors = require('colors')
 const schema = require('./schema/schema')
 require('dotenv').config();
+const connectDB = require('./config/db')
 const port = process.env.PORT || 3002
 
 // set express server
 const app = express();
+
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
   schema,
